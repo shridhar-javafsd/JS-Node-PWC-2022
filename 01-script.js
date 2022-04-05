43df11e9e3c1e7068efafe55d2fe9b9c53dcfafc
vamandeshmukh <vamandeshmukh@gmail.com>
2022-04-05 10:54:52 +0530

    node demo

--- 01-script.js
+++ 01-script.js
@@ -1,11 +1,37 @@
-// node 01-script.js 
+// on CMD run - 
+// npm i 
+// npm i express --save
 
-// NodeJS - js engine - v8 
-// NPM - 
+// to run - 
+// npm start 
+
+// to stop 
+// Ctrl + C
+
+const express = require('express');
+const app = express();
+const port = 9999;
+
+app.get('/', (request, response) => {
+    response.send('Welcome to my app ${port}');
+    console.log('App is running...');
+});
+
+app.listen(port, () => {
+    console.log('open http://localhost:9999/ to view the output.');
+});
+
+
+
+
+// node 01-script.js
+
+// NodeJS - js engine - v8
+// NPM -
 
 // create-react-app app-name
 
-// asychronous JS 
+// asychronous JS
 
 // console.log("One");
 
@@ -15,20 +41,21 @@
 
 // console.log("Three");
 
-// solutions 
+// solutions
 
-console.log("One");
+// console.log("One");
 
-getFun = (abc) => {
-    setTimeout(() => {
-        console.log("Two");
-        abc();
-    }, 2000);
-}
+// getFun = (abc) => {
+//     setTimeout(() => {
+//         console.log("Two");
+//         abc();
+//     }, 2000);
+// }
 
-fun = () => {
-    console.log("Three");
-}
+// fun = () => {
+//     console.log("Three");
+// }
 
-getFun(fun);
+// getFun(fun);
+
 
